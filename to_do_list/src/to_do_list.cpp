4e7be79a054858929eb0c479345b54445bef21c3
Kacper Grabowski <kacperg390@gmail.com>
2024-06-02 21:32:30 +0100

    Optimized code, fixed a bug where user is stuck in a loop when entering a character in the user menu and added ChangeFileDirectory function

--- to_do_list/src/to_do_list.cpp
+++ to_do_list/src/to_do_list.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
-#include <sstream>
-#include <fstream>
+#include <cstddef> // For size_t
+#include <sstream> // For getline input
+#include <fstream> // For creating files
 #include <vector>
+#include <filesystem> // For file directories
 
 template<typename T>
 static void Log(const T& MESSAGE) {
     std::cout << MESSAGE;
 }
 
-static void addToList(std::vector<std::string>& list) {
+static void AddToList(std::vector<std::string>& list) {
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     std::string task = "";
 
@@ -28,30 +30,34 @@ static void addToList(std::vector<std::string>& list) {
     }
 }
 
-static void showTheList(const std::vector<std::string>& LIST) {
+static void ShowTheList(const std::vector<std::string>& LIST) {
     Log('\n');
 
-    if (LIST.size() == 0) {
+    if (LIST.empty()) {
         Log("Your to-do list is empty.\n\n");
         return;
     }
 
-    short int taskNum = 1;
-    for (const std::string task : LIST) {
-        std::cout << taskNum << ". " + task << '\n';
+    size_t taskNum = 1;
+    for (const std::string TASK : LIST) {
+        std::cout << taskNum << ". " + TASK << '\n';
         taskNum++;
     }
     Log('\n');
 }
 
-static void saveToFile(const std::vector<std::string>& LIST) {
-    std::ofstream listFile("list.txt");
+static void SaveToFile(const std::vector<std::string>& LIST, std::string filePath) {
+    if (filePath != "list.txt") {
+        filePath = filePath + "/list.txt";
+    }
+
+    std::ofstream listFile(filePath);
 
     if (listFile.is_open()) {
         Log("\nFile opened successfully, writing to file...\n");
         
-        for (const std::string task : LIST) {
-            listFile << task << '\n';
+        for (const std::string TASK : LIST) {
+            listFile << TASK << '\n';
         }
         listFile.close();
         Log("File has been saved.\n\n");
@@ -61,11 +67,11 @@ static void saveToFile(const std::vector<std::string>& LIST) {
     }
 }
 
-static void deleteFromList(std::vector<std::string>& list) {
-    short int indexDel;
-    std::string userInput = "";
+static void DeleteFromList(std::vector<std::string>& list) {
+    size_t indexDel;
+    std::string userInput;
 
-    showTheList(list);
+    ShowTheList(list);
 
     while (!(list.empty())) {
         Log("\nEnter the task number you wish to delete (or enter 'Q' to go back): ");
@@ -75,36 +81,51 @@ static void deleteFromList(std::vector<std::string>& list) {
             return;
         }
 
-        indexDel = stoi(userInput) - 1;
+        indexDel = stoul(userInput) - 1;
 
-        if (indexDel < 0 || indexDel > (list.size() - 1)) {
-            Log("Enter a valid task number.");
-            showTheList(list);
+        if (indexDel >= 0 && indexDel < list.size()) {
+            list.erase(list.begin() + indexDel);
+            Log("Successfully deleted a task, updated list:");
+            ShowTheList(list);
             continue;
         }
-
-        list.erase(list.begin() + indexDel);
-        Log("Successfully deleted a task, updated list:");
-        showTheList(list);
-        continue;
+        else {
+            Log("Enter a valid task number.");
+            ShowTheList(list);
+            continue;
+        }
     }
     Log('\n');
-    Log("Your to-do list is empty.\n\n");
+}
+
+static void ChangeFileDirectory(std::string& changedFilePath) {
+    Log("Enter the directory path: cd ");
+    std::cin >> changedFilePath;
+
+    if (std::filesystem::exists(changedFilePath)) {
+        std::filesystem::current_path(changedFilePath);
+        std::ofstream listFile("list.txt");
+    }
+    else {
+        Log("The path does not exist.\n");
+    }
 }
 
 int main() {
     std::vector<std::string> list;
-    std::ifstream listFile("list.txt");
-    std::string line = "";
+    std::string filePath = "list.txt", line;
+    std::ifstream listFile(filePath);
 
     if (listFile.is_open()) {
+        Log("Opened your previous text file.\n");
+
         while (std::getline(listFile, line)) {
             list.push_back(line);
         }
         listFile.close();
     }
     else {
-        Log("Unable to open the txt file. A new file will be created upon saving.\n");
+        Log("Unable to open the text file.\n");
     }
     
     while (true) {
@@ -113,26 +134,40 @@ int main() {
         Log("\t2. Show to-do list\n");
         Log("\t3. Delete from to-do list\n");
         Log("\t4. Save the list to file\n");
-        Log("\t5. Quit the program\n\n");
+        Log("\t5. Open/create text file\n");
+        Log("\t6. Quit the program\n\n");
 
-        short int userChoice;
+        std::string userChoice;
+        size_t userNumberChoice = 0;
         Log("My choice is: ");
         std::cin >> userChoice;
 
-        switch (userChoice) {
+        if (userChoice == "1" || userChoice == "2" || userChoice == "3" ||
+            userChoice == "4" || userChoice == "5" || userChoice == "6") {
+            userNumberChoice = stoul(userChoice);
+        }
+        else {
+            Log("Invalid input, try again.\n\n");
+            continue;
+        }
+
+        switch (userNumberChoice) {
         case 1:
-            addToList(list);
+            AddToList(list);
             break;
         case 2:
-            showTheList(list);
+            ShowTheList(list);
             break;
         case 3:
-            deleteFromList(list);
+            DeleteFromList(list);
             break;
         case 4:
-            saveToFile(list);
+            SaveToFile(list, filePath);
             break;
         case 5:
+            ChangeFileDirectory(filePath);
+            break;
+        case 6:
             Log("Exiting...");
             return 1;
         default:
