43ba35a90f0cd5797f270a0719ff54871c3d1778
Kacper Grabowski <kacperg390@gmail.com>
2024-06-07 13:59:43 +0100

    Implemented changing file directory feature

--- to_do_list/src/to_do_list.cpp
+++ to_do_list/src/to_do_list.cpp
@@ -1,8 +1,10 @@
-#include <iostream>
-#include <cstddef> // For size_t
-#include <sstream> // For getline input
+#define NOMINMAX // Fixes a problem at line 29
+#include <Windows.h>
+
+#include <iostream> // For input/output operations
+#include <sstream> // For getline input and converting types
 #include <fstream> // For creating files
-#include <vector>
+#include <vector> // For dynamic array
 #include <filesystem> // For file directories
 
 template<typename T>
@@ -10,7 +12,20 @@ static void Log(const T& MESSAGE) {
     std::cout << MESSAGE;
 }
 
-static void AddToList(std::vector<std::string>& list) {
+static void showMenu() {
+    Log("==========\n");
+    Log("To-Do List\n");
+    Log("==========\n\n");
+    Log("Select an option:\n");
+    Log("\t1. Add to-do task\n");
+    Log("\t2. Show to-do list\n");
+    Log("\t3. Delete from to-do list\n");
+    Log("\t4. Save the list to file\n");
+    Log("\t5. Open/create text file\n");
+    Log("\t6. Quit the program\n\n");
+}
+
+static void addToVector(std::vector<std::string>& list) {
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     std::string task = "";
 
@@ -19,6 +34,7 @@ static void AddToList(std::vector<std::string>& list) {
         std::getline(std::cin, task);
 
         if (task == "Q" || task == "q") {
+            Log('\n');
             break;
         }
         else if (!(task.empty())) {
@@ -30,7 +46,7 @@ static void AddToList(std::vector<std::string>& list) {
     }
 }
 
-static void ShowTheList(const std::vector<std::string>& LIST) {
+static void showTheVectorList(const std::vector<std::string>& LIST) {
     Log('\n');
 
     if (LIST.empty()) {
@@ -38,7 +54,7 @@ static void ShowTheList(const std::vector<std::string>& LIST) {
         return;
     }
 
-    size_t taskNum = 1;
+    short int taskNum = 1;
     for (const std::string TASK : LIST) {
         std::cout << taskNum << ". " + TASK << '\n';
         taskNum++;
@@ -46,20 +62,16 @@ static void ShowTheList(const std::vector<std::string>& LIST) {
     Log('\n');
 }
 
-static void SaveToFile(const std::vector<std::string>& LIST, std::string filePath) {
-    if (filePath != "list.txt") {
-        filePath = filePath + "/list.txt";
-    }
+static void saveToFile(const std::vector<std::string>& LIST, const std::filesystem::path FILE_PATH) {
+    std::ofstream fileWriter(FILE_PATH);
 
-    std::ofstream listFile(filePath);
-
-    if (listFile.is_open()) {
+    if (fileWriter.is_open()) {
         Log("\nFile opened successfully, writing to file...\n");
-        
+
         for (const std::string TASK : LIST) {
-            listFile << TASK << '\n';
+            fileWriter << TASK << '\n';
         }
-        listFile.close();
+        fileWriter.close();
         Log("File has been saved.\n\n");
     }
     else {
@@ -67,11 +79,11 @@ static void SaveToFile(const std::vector<std::string>& LIST, std::string filePat
     }
 }
 
-static void DeleteFromList(std::vector<std::string>& list) {
-    size_t indexDel;
+static void deleteFromVector(std::vector<std::string>& list) {
+    short int indexDel;
     std::string userInput;
 
-    ShowTheList(list);
+    showTheVectorList(list);
 
     while (!(list.empty())) {
         Log("\nEnter the task number you wish to delete (or enter 'Q' to go back): ");
@@ -81,70 +93,140 @@ static void DeleteFromList(std::vector<std::string>& list) {
             return;
         }
 
-        indexDel = stoul(userInput) - 1;
+        indexDel = stoi(userInput) - 1;
 
         if (indexDel >= 0 && indexDel < list.size()) {
             list.erase(list.begin() + indexDel);
             Log("Successfully deleted a task, updated list:");
-            ShowTheList(list);
+            showTheVectorList(list);
             continue;
         }
         else {
             Log("Enter a valid task number.");
-            ShowTheList(list);
+            showTheVectorList(list);
             continue;
         }
     }
     Log('\n');
 }
 
-static void ChangeFileDirectory(std::string& changedFilePath) {
-    Log("Enter the directory path: cd ");
-    std::cin >> changedFilePath;
+static std::filesystem::path formatPath(std::string unformattefFilePath) {
+    // Gets the path and removes '\' at the start and at the end
+    size_t start = unformattefFilePath.find("\"") + 1;
+    size_t end = unformattefFilePath.find_last_of("\"");
+    std::string filePath = unformattefFilePath.substr(start, end - start);
 
-    if (std::filesystem::exists(changedFilePath)) {
-        std::filesystem::current_path(changedFilePath);
-        std::ofstream listFile("list.txt");
+    return filePath;
+}
+
+static void saveCurrentTextFilePath(std::filesystem::path& filePath) {
+    std::ofstream iniFileWriter("C:/Users/kacpe/Desktop/config.ini"); // Path to config file
+
+    if (iniFileWriter.is_open()) {
+        iniFileWriter << "[Settings]\n";
+        iniFileWriter << "LastUsedFilePath=" << filePath;
+        iniFileWriter.close();
+    }
+    else {
+        Log("Unable to open the file.");
+    }
+}
+
+static void importTasksFromFile(std::vector<std::string>& list, const std::filesystem::path FILE_PATH) {
+    std::ifstream textFileReader(FILE_PATH);
+
+    if (textFileReader.is_open()) {
+        Log("Opened text file at: ");
+        Log(FILE_PATH);
+        std::cout << std::endl << std::endl;
+
+        std::string line;
+        list.clear();
+        while (std::getline(textFileReader, line)) {
+            list.push_back(line);
+        }
+        textFileReader.close();
+    }
+    else {
+        Log("Unable to open the text file at: ");
+        Log(FILE_PATH);
+        Log(" check for the permissions of the file or if it exists. ");
+        std::cout << std::endl << std::endl;
+    }
+}
+
+static void openTextFilePath(std::vector<std::string>& list, std::filesystem::path& newFilePath) {
+    Log("Enter the directory path: cd ");
+    std::cin >> newFilePath;
+    newFilePath += "/list.txt";
+
+    // Read file
+    if (std::filesystem::exists(newFilePath)) {
+        importTasksFromFile(list, newFilePath);
+    }
+    // Create file
+    else if (std::filesystem::exists(newFilePath.replace_filename(""))) {
+        newFilePath += "list.txt";
+
+        std::ofstream createTextFile(newFilePath);
+        createTextFile.close();
+
+        Log("Created text file at: ");
+        Log(newFilePath);
+        std::cout << std::endl << std::endl;
+
+        list.clear();
     }
     else {
         Log("The path does not exist.\n");
     }
 }
 
-int main() {
-    std::vector<std::string> list;
-    std::string filePath = "list.txt", line;
-    std::ifstream listFile(filePath);
+static std::filesystem::path getExecutablePath() { // Needs to be changed
+    std::filesystem::path executablePath = std::filesystem::current_path();
+    return executablePath;
+}
 
-    if (listFile.is_open()) {
-        Log("Opened your previous text file.\n");
+static std::filesystem::path getLastUsedTextFilePath() {
+    std::ifstream iniFileReader("C:/Users/kacpe/Desktop/config.ini"); // Path to config file
 
-        while (std::getline(listFile, line)) {
-            list.push_back(line);
+    if (iniFileReader.is_open()) {
+        std::string line;
+        while (std::getline(iniFileReader, line)) {
+            if (line.find("LastUsedFilePath=") != std::string::npos) {
+                iniFileReader.close();
+
+                std::filesystem::path formattedFilePath = formatPath(line);
+
+                return formattedFilePath;
+            }
         }
-        listFile.close();
     }
     else {
-        Log("Unable to open the text file.\n");
+        Log("Unable to find last used text file path. Creating new file...");
     }
-    
+    // Used to create text file where this executable is if no path has been found
+    return getExecutablePath();
+}
+
+int main() {
+    std::filesystem::path filePath = getLastUsedTextFilePath();
+    std::vector<std::string> list;
+    importTasksFromFile(list, filePath);
+
     while (true) {
-        Log("Select an option:\n");
-        Log("\t1. Add to to-do list\n");
-        Log("\t2. Show to-do list\n");
-        Log("\t3. Delete from to-do list\n");
-        Log("\t4. Save the list to file\n");
-        Log("\t5. Open/create text file\n");
-        Log("\t6. Quit the program\n\n");
+        showMenu();
 
         std::string userChoice;
-        size_t userNumberChoice = 0;
-        Log("My choice is: ");
+        short int userNumberChoice = 0;
+        Log(filePath);
+        Log("> My choice is: ");
         std::cin >> userChoice;
 
         if (userChoice == "1" || userChoice == "2" || userChoice == "3" ||
-            userChoice == "4" || userChoice == "5" || userChoice == "6") {
-            userNumberChoice = stoul(userChoice);
+            userChoice == "4" || userChoice == "5" || userChoice == "6" ||
+            userChoice == "7") {
+            userNumberChoice = stoi(userChoice);
         }
         else {
             Log("Invalid input, try again.\n\n");
@@ -153,22 +235,22 @@ int main() {
 
         switch (userNumberChoice) {
         case 1:
-            AddToList(list);
+            addToVector(list);
             break;
         case 2:
-            ShowTheList(list);
+            showTheVectorList(list);
             break;
         case 3:
-            DeleteFromList(list);
+            deleteFromVector(list);
             break;
         case 4:
-            SaveToFile(list, filePath);
+            saveToFile(list, filePath);
             break;
         case 5:
-            ChangeFileDirectory(filePath);
+            openTextFilePath(list, filePath);
+            saveCurrentTextFilePath(filePath);
             break;
         case 6:
-            Log("Exiting...");
             return 1;
         default:
             Log("Invalid input, try again.\n\n");
